10ecb4119b51b18960f8d6e4a3cb9c248a8c7119
triest <you@example.com>
2020-10-12 21:57:38 +0300

    Fix sql file

--- query.sql
+++ query.sql
@@ -1,6 +1,4 @@
-use jedais;
-
-select jeda.name,count(padawa.jedi_id)
+select jeda.name,count(padawa.jedi_id) as "padavan_count"
 	from jedi jeda
 		left join padawan padawa on padawa.jedi_id=jeda.id
-			group by padawa.jedi_id
+			group by padawa.jedi_id  ORDER BY padavan_count DESC
